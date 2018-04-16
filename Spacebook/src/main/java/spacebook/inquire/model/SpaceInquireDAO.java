c7b515198c7bdb5264d9ca8875ed9121682ec7ec
LeeHyewoo <LeeHyewoo@DESKTOP-Q01CD2R>
2018-04-16 17:38:28 +0900

    혜우 - 1:1문의관련 package 수정, def.xml수정

--- Spacebook/src/main/java/spacebook/inquire/model/SpaceInquireDAO.java
+++ Spacebook/src/main/java/spacebook/inquire/model/SpaceInquireDAO.java
@@ -3,11 +3,12 @@ package spacebook.inquire.model;
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 
 public class SpaceInquireDAO extends SqlSessionDaoSupport {
-	public void spaceInquireInsert(SpaceInquireDTO dto) {
-		getSqlSession().insert("spaceReview.insertReview", dto);
+
+	public void spaceInquireInsert(SpaceInquireDTO inquireDTO) {
+		getSqlSession().insert("spaceInquire.insertInquire", inquireDTO);
 	}
-	
-	public void spaceInquireDelete(SpaceInquireDTO dto) {
-		getSqlSession().delete("spaceReview.insertReview", dto);
+
+	public void spaceInquireDelete(SpaceInquireDTO inquireDTO) {
+		getSqlSession().delete("spaceInquire.deleteInquire", inquireDTO);
 	}
 }
--- Spacebook/src/main/java/spacebook/inquire/service/SpaceInquireService.java
+++ Spacebook/src/main/java/spacebook/inquire/service/SpaceInquireService.java
@@ -1,8 +1,9 @@
 package spacebook.inquire.service;
 
-import spacebook.view.model.SpaceInquireDTO;
+import spacebook.inquire.model.SpaceInquireDTO;
 
 public interface SpaceInquireService {  
+	
 	public void insertSpaceInquire(SpaceInquireDTO dto);
 	
 	public void deleteSpaceInquire(SpaceInquireDTO dto);
